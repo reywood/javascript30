abfb177ab874445af8792638dd31a39ace244131
Sean Dwyer <sean.dwyer@gmail.com>
2017-04-17 20:38:42 -0700

    Fix

--- 16 - Mouse Move Shadow/script.js
+++ 16 - Mouse Move Shadow/script.js
@@ -11,8 +11,8 @@ function shadow(e) {
         y += e.target.offsetTop;
     }
 
-    xWalk = Math.round((x / width * walk) - (walk / 2));
-    yWalk = Math.round((y / height * walk) - (walk / 2));
+    const xWalk = Math.round(((x / width) * walk) - (walk / 2));
+    const yWalk = Math.round(((y / height) * walk) - (walk / 2));
 
     text.style.textShadow = `
         ${xWalk}px ${yWalk}px 0 rgba(255, 0, 255, .7),
